89513d443e089a336de017cca0e630145c13e510
Vishal <vishal@bluebash.co>
2018-11-27 18:23:44 +0530

    Added deface overriding when no data-hook present with exiting records

--- app/overrides/add_import_button_in_admin_tax_rates_page.rb
+++ app/overrides/add_import_button_in_admin_tax_rates_page.rb
@@ -3,3 +3,10 @@ Deface::Override.new(
   name:          'add_import_button_in_admin_tax_rates_page',
   insert_bottom:  '[class="table"]',
   partial: "spree/admin/shared/import_tax_rates")
+
+
+Deface::Override.new(
+  virtual_path:  'spree/admin/tax_rates/index',
+  name:          'add_import_button_in_admin_tax_rates_page_when_no_rates',
+  insert_bottom:  '[class="alert alert-info no-objects-found"]',
+  partial: "spree/admin/shared/import_tax_rates")
